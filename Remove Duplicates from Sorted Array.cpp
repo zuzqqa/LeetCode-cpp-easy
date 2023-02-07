2457e5b9aae2222b4375bad5679e855bdd48463f
zuzqqa <zuzannanowak04@wp.pl>
2023-02-07 22:04:13 +0100

    Create Remove Duplicates from Sorted Array.cpp

---
+++ Remove Duplicates from Sorted Array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+
+        auto itr = nums.begin();
+        
+        while( itr + 1 != nums.end() ){
+            if( *itr == *(itr+1) ) itr = nums.erase(itr);
+            else itr++;
+        }
+        
+        return nums.size();
+    }
+};
