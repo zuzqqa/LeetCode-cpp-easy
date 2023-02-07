c11f9944710160edbe92fb315847686a5973ec34
zuzqqa <zuzannanowak04@wp.pl>
2023-02-07 19:39:27 +0100

    Create Remove Element.cpp

---
+++ Remove Element.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums2, int val) {
+        int i = 0;
+
+        for( vector<int>::iterator itr = nums2.begin(); itr != nums2.end(); itr++ ){
+            if ( *itr != val ) {
+               nums2[i] = *itr; 
+               i++;
+            }
+        }
+        return i;
+    }
+};
