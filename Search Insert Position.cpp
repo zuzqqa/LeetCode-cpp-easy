1b9359252b9574163a7541ba629ff211a34a7845
zuzqqa <zuzannanowak04@wp.pl>
2023-02-07 21:49:14 +0100

    Create Search Insert Position.cpp

---
+++ Search Insert Position.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        auto itr = lower_bound(nums.begin(), nums.end(), target);
+        
+        if( itr != nums.end() ) return distance(nums.begin(), itr);
+
+        nums.push_back(target);
+
+        sort(nums.begin(), nums.end());
+
+        itr = lower_bound(nums.begin(), nums.end(), target);
+
+        return distance(nums.begin(), itr);
+    }
+};
